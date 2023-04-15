f4fb54318b03f1578bb5b557878e224c60d46db5
uimarshall <uimarshall@gmail.com>
2023-04-15 02:53:09 +0100

    Add configurations for tailwind & github workflows

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <p>my portfolio</p>
+        <h1 className="text-3xl font-bold underline">My Portfolio!</h1>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
